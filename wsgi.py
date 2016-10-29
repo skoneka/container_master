f625cb620b54a445c79b2382f757cfc955eff90f
Artur Skonecki <artur.skonecki@domo-safety.com>
2016-10-29 22:50:08 +0200

    container up

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def container_up():
+    container = request.args['container']
+    time = request.args['time']
+    return "container %s timestamp %s" % (container, time)
 
 if __name__ == "__main__":
     application.run()
