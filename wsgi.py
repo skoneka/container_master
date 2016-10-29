5a349eaf1e4f444c1927bdf63a97061c956ac952
Artur Skonecki <artur.skonecki@domo-safety.com>
2016-10-29 23:30:48 +0200

    container up get timestamp and address

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
+from time import time
 application = Flask(__name__)
 
 @application.route("/")
 def container_up():
-    container = request.args['container']
-    time = request.args['time']
-    return "container %s timestamp %s" % (container, time)
+    epoch_time = time()
+    container = request.remote_addr
+    status = "container %s timestamp %f" % (container, epoch_time)
+    print (status)
+    return status
 
 if __name__ == "__main__":
     application.run()
