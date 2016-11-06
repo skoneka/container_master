565f5fb26ba8819c9b2e59151168c8f7a0c371f0
Artur Skonecki <artur.skonecki@domo-safety.com>
2016-11-06 17:09:39 +0100

    instructions

--- wsgi.py
+++ wsgi.py
@@ -5,19 +5,12 @@ from subprocess import Popen
 from os import getloadavg
 from myutil import get_meminfo
 
-#startmeup:
-# rsync -avx --delete /media/a/355fc64b-d7c7-4a25-9d43-ffa8fdf9275f/hipopotam/home/a/Dropbox_skoneka/Dropbox/master/measure-images root@docker2:~/
-# cd measure-images/container_master
-# bash start-local.sh
-# systemctl restart docker.service; oc cluster up
-# time oc new-app https://github.com/skoneka/container_testme.git --name "testme-image"
-# oc scale --replicas=0 dc/testme-image
-# curl '159.100.252.59:8080/timestamp?comment=START' && curl '159.100.252.59:8080/container_next?count=1&comment="first+container"'
 
 
 
-DOCKER_REPO_URL="172.30.18.89:5000/myproject/testme-image:latest"
-MEM_RAM_THRESHOLD=2900000
+
+DOCKER_REPO_URL="REPLACEME_DOCKER_REPO_URL"
+MEM_RAM_THRESHOLD=100000
 
 application = Flask(__name__)
 flog = open ("/tmp/containerup.log", "a")
@@ -54,6 +47,7 @@ def url_container_next():
         print(status, file=flog); flog.flush()
         return status, 417
     else:
+        print(status, file=flog); flog.flush()
         count = int(request.args.get('count'))
         Popen("""time oc new-app "%s" --name "testme-%d" --insecure-registry""" % (DOCKER_REPO_URL, count), shell=True).wait()
         return status, 201
@@ -65,20 +59,6 @@ def url_container_next():
 #def url_test():
 #    print(request.args)
 #    return "test",200
-
-
-  
-#@application.route("/container_start", methods=['GET'])
-#def url_container_start():  
-#    count = int(request.args.get('count'))
-#    Popen("""time oc new-app "%s" --name "testme-%d" --insecure-registry""" % (DOCKER_REPO_URL, count), shell=True).wait()
-#    
-#    return "container_start", 200
-
-# print available cluster memory here too
-# should container get their startup time to get delays for  single containers? or just use simple scale to time different numbers of containers being created at once
-
-# 
 
 if __name__ == "__main__":
     application.run()
